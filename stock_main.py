d6393dad54e2931a90fc2fd126c068e63dda2172
ylliu <ylliu@qq.com>
2025-06-17 23:17:45 +0800

    cookie过期直接退出

--- stock_main.py
+++ stock_main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         if isinstance(stocks, str):
             stock_monitor.send_message2("系统异常，请检查cookie是否过期", stocks)
             stock_monitor.send_message2_wechat("系统异常，请检查cookie是否过期", stocks)
-            time.sleep(60)
+            break
         add_stocks = stock_monitor.get_add_stocks(stocks, previous)
         remove_stocks = stock_monitor.get_remove_stocks(stocks, previous)
         stock_monitor.send_message(add_stocks, remove_stocks)
@@ -23,6 +23,6 @@ if __name__ == "__main__":
         current_hour = current_time.hour
         current_minute = current_time.minute
         if current_hour in send_times and current_minute == 0:  # 确保是整点
-            stock_monitor.send_message2("轮回666小助手在线中", "")
-            stock_monitor.send_message2_wechat("轮回666小助手在线中", stocks)
+            stock_monitor.send_message2("薛定谔的牛小助手在线中", "")
+            stock_monitor.send_message2_wechat("薛定谔的牛小助手在线中", stocks)
             time.sleep(60)
